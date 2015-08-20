5f162e6c6e5f0fffa1b650f7c518db35fc567f07
Hemstreet <jonhemstreet@gmail.com>
2015-08-20 17:16:45 -0600

    initial commit

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-(function() {
+var _ = require('lodash');
 
+var obj = {
 
+    hello: "World",
 
-})();
+    init: function(config) {
+
+        //var config = require('./config/config.json');
+
+        var config = _.assign( config, {
+            "test": true
+        });
+
+        console.log(config, this.hello);
+
+    }
+};
+
+obj.init({
+    "test": false
+});
